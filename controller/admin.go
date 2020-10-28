f72699087fb6d01c37eb2c07417d60bfff72629f
yguilai <1007139643@qq.com>
2020-10-28 10:09:11 +0800

    project init

---
+++ controller/admin.go
@@ -0,0 +1,15 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+type AdminController struct {
+}
+
+func RegisterAdminController(r *gin.RouterGroup)  {
+	//admin := &AdminController{}
+	//r.POST("/login")
+}
+
+func (admin *AdminController) AdminLogin(c *gin.Context)  {
+
+}
